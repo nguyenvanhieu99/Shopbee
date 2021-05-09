340c3754d07ba9bbb30956d9026b2a0007491626
VanHieu <nvhieu.dev@gmail.com>
2021-05-09 11:18:49 +0700

    change to maven project

--- src/java/Controller/show_Siteadmin_TK.java
+++ src/java/Controller/show_Siteadmin_TK.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import static jdk.nashorn.internal.runtime.Debug.id;
+//import static jdk.nashorn.internal.runtime.Debug.id;
 
 /**
  *
